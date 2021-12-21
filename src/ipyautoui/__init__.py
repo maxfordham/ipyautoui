d0f8aa70f7eb2d2602ca8fb26a312742f702bfb8
john <j.gunstone@maxfordham.com>
2021-12-21 16:08:46 +0000

    housekeeping

--- src/ipyautoui/__init__.py
+++ src/ipyautoui/__init__.py
@@ -4,5 +4,5 @@ import sys
 sys.path.append(str(pathlib.Path(__file__).parents[1]))
 #  ^ for dev only. TODO: comment out at build time
 
-from ipyautoui.autoui import AutoUi
+from ipyautoui.autoui import AutoUi, AutoUiConfig
 from ipyautoui.displayfile import DisplayFiles
