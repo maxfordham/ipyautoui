c7ad43d1d8f162c0288fee0fc23a3b5c044c6df1
john <j.gunstone@maxfordham.com>
2021-12-23 15:56:34 +0000

    updated to make more configurable

--- src/ipyautoui/custom/modelrun.py
+++ src/ipyautoui/custom/modelrun.py
@@ -1,50 +1,154 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.3
+#   kernelspec:
+#     display_name: Python [conda env:ipyautoui]
+#     language: python
+#     name: conda-env-ipyautoui-xpython
+# ---
+
 import ipywidgets as widgets
 from traitlets import HasTraits, Unicode, default, validate, TraitError
 import re
+from dataclasses import dataclass, asdict
+import typing
 
-class ModelRunName(widgets.HBox, HasTraits):
-    """widget for creating an modelling iteration name to a defined format from component parts"""
+
+@dataclass
+class RunNameInputs:
+    index: int = 1
+    disabled_index: bool = False
+    zfill: int = 2
+    enum: typing.List = None
+    delimiter: str = '-'
+    description_length: int = None
+    allow_spaces: bool = False
+    order: tuple = ('index', 'enum', 'description')
+    pattern: str = None
+
+    def __post_init__(self):
+        li = list(self.order)
+        if self.index is None:
+            li = [l for l in li if l!='index'] 
+        if self.enum is None:
+            li = [l for l in li if l!='enum'] 
+        if self.description_length is None:
+            li = [l for l in li if l!='description'] 
+        self.order = tuple(li)
+        if len(self.order) < 1:
+            raise ValueError('you must include 1 of index, enum or description')
+        
+        di={}
+        di['index'] = "[0-9]" * self.zfill + f"[{self.delimiter}]"
+        di['enum'] = f"[a-z,A-Z,0-9]*[{self.delimiter}]"
+        di['description'] = f".+[{self.delimiter}]"
+        p = ""
+        for l in self.order:
+            try:
+                p += di[l]
+            except:
+                pass
+        self.pattern = p[:-3]      
+
+
+class RunName(widgets.HBox, HasTraits):
+    """widget for creating an modelling iteration name to a defined format from component parts
+    
+    Example:
+        value = '000-lean-short_description_of_model-run'
+        enum = ['lean', 'clean', 'green']
+        zfill = 2
+    """
     value = Unicode()
     
     @validate('value')
     def _valid_value(self, proposal):
         val = proposal['value']
-        matched = re.match("[0-9][0-9][0-9][-][a-z,A-Z,0-9]*[-].+", val)
+        matched = re.match(self.inputs.pattern, val) #
         if not bool(matched):
-            raise TraitError('string musts have format: "[0-9][0-9][0-9][-][a-z,A-Z,0-9]*[-].+" , e.g. 000-lean-short_run_description_of_model_run')
+            print(self.inputs.pattern)
+            print(val)
+            raise TraitError(f'string musts have format: {self.inputs.pattern}')#
         return val
     
-    def __init__(self, value='000-lean-short_description_of_model-run', enum=['lean', 'clean', 'green'], disabled_number=False):
-        self.value = value
-        self.enum = enum
-        self.disabled_number = disabled_number
-        self._init_form()
+    def __init__(self, value = None,
+                    index: int = 1,
+                    disabled_index: bool = True,
+                    zfill: int = 2,
+                    enum: typing.List = ['lean','clean', 'green'],
+                    delimiter: str = '-',
+                    description_length: int = 30,
+                    allow_spaces: bool = False,
+                    order=('index', 'enum', 'description')
+                ):
+        di = {k:v for k, v in locals().items() if k != 'value' and k != 'self' and k!='__class__'}
+        super().__init__()
+        self.inputs = RunNameInputs(**di)
+        if value is not None:
+            self.value = value
+        self._init_RunName()
         self._init_controls()
         self.update_name('change')
-    
-    def _set_enum(self):
-        if 'enum' in self.kwargs:
-            self.enum = self.kwargs['enum']
+        
+    @property
+    def get_options(self):
+        if self.inputs.enum is None:
+            return []
         else:
-            self.enum = ['lean', 'clean', 'green']
-            
-    def _init_form(self):
-        number, runtype_enum, run_description = self.value.split('-', 2)
-        self.number = widgets.IntText(value=number,layout={'width':'50px'}, disabled=self.disabled_number)
-        self.runtype_enum = widgets.Dropdown(value=runtype_enum, options=self.enum, layout={'width':'100px'})
-        self.run_description = widgets.Text(value=run_description)
+            return self.inputs.enum 
+        
+    def _init_RunName(self):
+        try:
+            index, enum, description = self.value.split(self.inputs.delimiter, self.inputs.sections)
+        except:
+            index, enum, description = None, None, 'description'
+        self.index = widgets.IntText(value=index,layout={'width':'50px'}, disabled=self.inputs.disabled_index)
+        self.enum = widgets.Dropdown(value=enum, options=self.get_options, layout={'width':'100px'})
+        self.description = widgets.Text(value=description)
         self.name = widgets.Text(disabled=True)
-        super().__init__(
-            children=[self.number, self.runtype_enum, self.run_description, self.name],
-        )
+        di = {}   
+        di['index'] = self.index
+        di['enum'] = self.enum
+        di['description'] = self.description
+        children = []
+        for l in self.inputs.order:
+            try:
+                children.append(di[l])
+            except:
+                pass
+        self.children = children + [self.name]
         
     def _init_controls(self):
-        self.number.observe(self.update_name, 'value')
-        self.runtype_enum.observe(self.update_name, 'value')
-        self.run_description.observe(self.update_name, 'value')
-
+        self.index.observe(self.update_name, names='value')
+        self.enum.observe(self.update_name, names='value')
+        self.description.observe(self.update_name, names='value')
+        
     def update_name(self, on_change):
-        self.name.value = str(self.number.value).zfill(3) \
-            + '-' + self.runtype_enum.value \
-            + '-' + self.run_description.value.replace(' ','_')
+        di = {}
+        di['enum'] = str(self.enum.value) + self.inputs.delimiter
+        di['index'] = str(self.index.value).zfill(self.inputs.zfill) + self.inputs.delimiter
+        di['description'] = self.description.value.replace(self.inputs.delimiter,'_') + self.inputs.delimiter
+        if not self.inputs.allow_spaces:
+            di['description'] = di['description'].replace(' ','_')
+        if self.inputs.description_length is not None:
+            di['description'] = di['description'][0:self.inputs.description_length]
+        v = ""
+        for l in self.inputs.order:
+            try: 
+                v += di[l]
+            except:
+                pass
+        self.name.value = v[:-1]   
         self.value = self.name.value
+
+
+if __name__ == "__main__":
+    run = RunName()
+    display(run)
