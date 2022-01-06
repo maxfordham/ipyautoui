44e59cc4ad634da65c07d4292f9f95772865b5f3
john <j.gunstone@maxfordham.com>
2022-01-06 13:50:19 +0000

    name update for iterables

--- src/ipyautoui/custom/__init__.py
+++ src/ipyautoui/custom/__init__.py
@@ -6,6 +6,6 @@ sys.path.append(str(pathlib.Path(__file__).parents[2]))
 from ipyautoui.custom.grid import Grid
 from ipyautoui.custom.filechooser import FileChooser
 from ipyautoui.custom.multiselect_search import MultiSelectSearch
-from ipyautoui.custom.iterable import VArray, HArray, VDictionary, HDictionary
+from ipyautoui.custom.iterable import Array, Dictionary
 from ipyautoui.custom.modelrun import RunName
 from ipyautoui.custom.loadproject import LoadProject
