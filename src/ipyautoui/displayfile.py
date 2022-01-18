e4fc196ab57211640c01e8106c3323970fe2b904
olly <o.hensby@maxfordham.com>
2022-01-18 15:47:31 +0000

    Created create_notebooks.sh which creates notebooks from specified python files

--- src/ipyautoui/displayfile.py
+++ src/ipyautoui/displayfile.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.5
+#       jupytext_version: 1.13.6
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
