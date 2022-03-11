f63da983901aa816b1ac58ff6fcccde611d97cf5
olly <o.hensby@maxfordham.com>
2022-03-11 16:20:13 +0000

    removed other python versions. Kept 3.9

--- setup.py
+++ setup.py
@@ -26,9 +26,6 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ]
 )
