615c16426bddbdb771031429fcfdc1e1059e826c
john <j.gunstone@maxfordham.com>
2022-04-19 11:37:31 +0100

    added file method

--- src/ipyautoui/basemodel.py
+++ src/ipyautoui/basemodel.py
@@ -1,6 +1,24 @@
 """extending default pydantic BaseModel. NOT IN USE."""
 import pathlib
 from pydantic import BaseModel
+from typing import Type
+
+def file(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
+    """
+    this is a method that is added to the pydantic BaseModel within AutoUi using
+    "setattr".
+    
+    Example:
+        ```setattr(self.config_autoui.pydantic_model, 'file', file)```
+        
+    Args:
+        self (pydantic.BaseModel): instance
+        path (pathlib.Path): to write file to
+    """
+    if "indent" not in json_kwargs.keys():
+        json_kwargs.update({"indent": 4})
+    path.write_text(self.json(**json_kwargs), encoding="utf-8")
+    
 
 class BaseModel(BaseModel):
     def file(self, path: pathlib.Path, **json_kwargs):
