3917ffffa8f28613a24bf97c284faf18e026ad99
jgunstone <gunstone.john@gmail.com>
2022-05-13 15:21:39 +0100

    black formatting

--- src/ipyautoui/autovjsf.py
+++ src/ipyautoui/autovjsf.py
@@ -18,13 +18,13 @@
 # %load_ext lab_black
 
 # uncomment next lines to enable hot reloading of vue template(s). (needs the watchdog package)
-import ipywidgets as widgets
 import ipyvue
 import ipyvuetify as v
 import traitlets
 from ipyautoui.constants import PATH_VJSF_TEMPLATE
 
 ipyvue.watch(".")
+
 
 class AutoVjsf(v.VuetifyTemplate):
     template_file = str(PATH_VJSF_TEMPLATE)
@@ -33,15 +33,14 @@ class AutoVjsf(v.VuetifyTemplate):
     schema = traitlets.Dict().tag(sync=True)
     valid = traitlets.Bool(False).tag(sync=True)
 
-
 # -
 
 if __name__ == "__main__":
     from ipyautoui.test_schema import TestAutoLogic
+    from IPython.display import display
 
     test = TestAutoLogic()
     sch = test.schema()
-    ui = AutoVjsf1(schema=sch)
+    ui = AutoVjsf(schema=sch)
     display(ui)
-
 
