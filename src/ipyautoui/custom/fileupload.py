c046210a86a0d3a6332534add05c883f8cb73cc0
john <j.gunstone@maxfordham.com>
2022-05-27 11:13:13 +0100

    updated. tidid. tested. now working.

--- src/ipyautoui/custom/fileupload.py
+++ src/ipyautoui/custom/fileupload.py
@@ -1,43 +1,33 @@
+# +
 """file upload wrapper"""
 # %load_ext lab_black
 import ipywidgets as widgets
-from ipyautoui.constants import BUTTON_MIN_SIZE
-from ipyautoui._utils import open_file
-
-class FileUpload(widgets.FileUpload):
-    """file upload wrapper"""
-
-    def __init__(self, value: None = None, path=None, **kwargs):
-        super().__init__(**kwargs)
-
-
-f = FileUpload()
-
-# +
-from ipyautoui.autodisplay import AutoDisplay, DisplayObject
-
-# #  ?DisplayObject.from_path
-
-# +
-
-BUTTON_MIN_SIZE
-# -
-
-
-
-
-
-# +
+from markdown import markdown
+from IPython.display import display
 from pydantic import BaseModel, validator, Field
 import pathlib
 import typing
-from ipyautoui.automapschema import attach_schema_refs
-from ipyautoui.custom.iterable import Array, AutoArray, Dictionary
 import stringcase
 from datetime import datetime
+import traitlets
+import json
+import traitlets_paths
 
+from ipyautoui.automapschema import attach_schema_refs
+from ipyautoui.constants import BUTTON_MIN_SIZE, KWARGS_OPENFOLDER
+from ipyautoui._utils import open_file, obj_to_importstr, getuser
+from ipyautoui.autodisplay import AutoDisplay, DisplayObject
+from ipyautoui.custom.iterable import Array, AutoArray, Dictionary
 
-class UploadedFile(BaseModel):
+# +
+# TODO: allow for adding number of allowed files based on schema
+# TODO: patterns for types etc.
+# TODO: create base object without description and then extend. OR allow for prepopulated and disabled description
+# TODO: add "required files" to the upload that detects name and type
+# TODO: add optional description to linked files
+# -
+
+class File(BaseModel):
     name: str
     type: str
     last_modified: datetime
@@ -62,73 +52,16 @@ class UploadedFile(BaseModel):
         return values["fdir"] / values["name"]
 
 
-def read_file_upload_item(di: dict, fdir=pathlib.Path(".")):
-    _ = di["metadata"]
-    _["fdir"] = fdir
-    f = UploadedFile(**_)
-    return f
-
-
-def add_files(upld_value, fdir=pathlib.Path(".")):
-    di = {}
-    for k, v in upld_value.items():
-        f = read_file_upload_item(v, fdir=fdir)
-        f.path.write_bytes(v["content"])
-        di[k] = f
-    return di
-
-
-class Files(BaseModel):
-    fdir: pathlib.Path = pathlib.Path(".")
-    files: typing.Dict[str, UploadedFile] = Field(default_factory=lambda: {})
-
-
-# -
-
-Files()
-
-# +
-from ipyautoui._utils import obj_to_importstr
-
-obj_to_importstr(FileUploadToDir)
-# -
-
-
-
-d = Dictionary(add_remove_controls="remove_only", show_hash=None)
-d.add_row
-
-upld = widgets.FileUpload(multiple=True)
-upld
-
-value = upld.value
-
-# # ?AutoArray
-sch = attach_schema_refs(Files.schema())
-# sch["properties"]["files"]
-
-Files.schema()["properties"]
-
-Files()
-
-f = read_file_upload_item(upld.value["README.txt"])
-
-# +
-import traitlets
-import json
-import traitlets_paths
-
-
-class UploadedFileUi(widgets.HBox):
+class FileUi(widgets.HBox):
     _value = traitlets.Dict()
 
     # @traitlets.validate("_value")
     # def _valid_value(self, proposal):
-    #     return json.loads(UploadedFile(**proposal["value"]).json())
+    #     return json.loads(File(**proposal["value"]).json())
 
-    def __init__(self, value: typing.Union[dict, UploadedFile]):
+    def __init__(self, value: typing.Union[dict, File]):
         self._init_form()
-        if isinstance(value, UploadedFile):
+        if isinstance(value, File):
             value = json.loads(value.json())
         self.value = value
         self._init_controls()
@@ -157,18 +90,43 @@ class UploadedFileUi(widgets.HBox):
 
 
 # +
+def read_file_upload_item(di: dict, fdir=pathlib.Path("."), added_by=None):
+    if added_by is None:
+        added_by = getuser()
+    _ = di["metadata"]
+    _["fdir"] = fdir
+    _["added_by"] = added_by
+    f = File(**_)
+    return f
+
+def add_files(upld_value, fdir=pathlib.Path(".")):
+    if not pathlib.Path(fdir).exists():
+        pathlib.Path(fdir).mkdir(exist_ok=True)
+    di = {}
+    for k, v in upld_value.items():
+        f = read_file_upload_item(v, fdir=fdir)
+        f.path.write_bytes(v["content"])
+        di[k] = f
+    return di
+
+
 class FileUploadToDir(widgets.VBox):
-    _value = traitlets.Dict(default_value=[])
+    _value = traitlets.Dict(default_value={})
+    fdir = traitlets.Unicode()
 
-    def __init__(self, schema=None, value=None):
-
+    def __init__(
+        self,
+        schema=None,
+        value: typing.Union[typing.Dict[str, File], dict] = None,
+        fdir="linked_files",
+    ):
+        self.fdir = fdir
         self._init_form()
         self._init_controls()
         if value is None:
-            value = Files()
+            value = {}
 
     def fn_remove(self, key=None):
-
         path = pathlib.Path(self.fdir) / key
         path.unlink()
 
@@ -176,40 +134,44 @@ class FileUploadToDir(widgets.VBox):
     def value(self):
         return self._value
 
+    @staticmethod
+    def convert_to_dict(item):
+        if isinstance(item, File):
+            return json.loads(value.json())
+        else:
+            return item
+
     @value.setter
     def value(self, value):
-        if isinstance(value, Files):
-            value = json.loads(value.json())
-        if value == {}:
-            value = json.loads(Files().json())
+        value = {k: self.convert_to_dict(v) for k, v in value.items()}
+        for k, v in value.items():
+            if v["fdir"] != self.fdir:
+                raise ValueError(f"fdir for {k} != {self.fdir}")
         self.arr_files.items = {}
-        self.fdir = value["fdir"]
-        self.add_files(value["files"])
+        self.add_files(value)
         self._arr_files("change")
-        # self._value = value
-        # print(self.value)
 
     def _init_form(self):
         super().__init__()
+        self.hbx_buttons = widgets.HBox()
         self.upld = widgets.FileUpload(multiple=True)
+        self.text = widgets.HTML(markdown(f"`{self.fdir}`"))
+        self.hbx_buttons.children = [self.upld, self.text]
         self.arr_files = Dictionary(
             add_remove_controls="remove_only", show_hash=None, fn_remove=self.fn_remove
         )
-        self.children = [self.upld, self.arr_files]
+        self.children = [self.hbx_buttons, self.arr_files]
 
     def _init_controls(self):
         self.upld.observe(self._upld, names="value")
         self.arr_files.observe(self._arr_files, names="value")
 
     def _arr_files(self, onchange):
-        self._value = json.loads(
-            Files(fdir=self.fdir, files=self.arr_files.value).json()
-        )
-        print(self.value)
+        self._value = self.arr_files.value
 
     def add_files(self, files):
         for k, v in files.items():
-            ui = UploadedFileUi(v)
+            ui = FileUi(v)
             self.arr_files.add_row(item=ui, new_key=k)
 
     def _upld(self, onchange):
@@ -218,130 +180,50 @@ class FileUploadToDir(widgets.VBox):
         self.upld._counter = 0
 
 
-upld = FileUploadToDir()
+if __name__ == "__main__":
+    upld = FileUploadToDir()
+    display(upld)
 # -
 
-upld
+if __name__ == "__main__":
+    from ipyautoui import AutoUi
 
-json.loads(Files(fdir=".", files={}).json())
+    class Ui(BaseModel):
+        name: str
+        description: str
+        files: typing.Dict[str, File] = Field(
+            autoui="__main__.FileUploadToDir", maximumItems=1, minimumItems=0
+        )
 
-from ipyautoui import AutoUi
+    aui = AutoUi(schema=Ui)
+    display(aui)
 
-
-# +
-class Ui(BaseModel):
-    name: str
-    description: str
-    files: Files = Field(autoui="__main__.FileUploadToDir")
-
-
-aui = AutoUi(schema=Ui)
-aui
-# -
-
-upld.value = {
-    "fdir": ".",
-    "files": {
-        "make.bat": {
-            "name": "make.bat",
-            "type": "",
-            "last_modified": "2022-03-23T15:27:13.536000",
-            "size": 896,
-            "fdir": ".",
-            "path": "make.bat",
-            "caption": None,
-            "added_by": None,
+if __name__ == "__main__":
+    aui.value = {
+        "name": "file collection",
+        "description": "about stuff",
+        "files": {
+            "make.bat": {
+                "name": "make.bat",
+                "type": "",
+                "last_modified": "2022-03-23T15:27:13.536000",
+                "size": 896,
+                "fdir": ".",
+                "path": "make.bat",
+                "caption": None,
+                "added_by": None,
+            },
+            "Makefile": {
+                "name": "Makefile",
+                "type": "",
+                "last_modified": "2022-03-23T15:27:13.518000",
+                "size": 654,
+                "fdir": ".",
+                "path": "Makefile",
+                "caption": None,
+                "added_by": None,
+            },
         },
-        "Makefile": {
-            "name": "Makefile",
-            "type": "",
-            "last_modified": "2022-03-23T15:27:13.518000",
-            "size": 654,
-            "fdir": ".",
-            "path": "Makefile",
-            "caption": None,
-            "added_by": None,
-        },
-    },
-}
-
-# +
-import getpass
-
-user = getpass.getuser()
-user
-# -
-
-upld.value
-
-upld.fn_remove(key="README.txt")
-
-upld.value
-
-os.environ
+    }
 
 
-
-# +
-
-pathlib.Path(upld.value[key]["path"]).unlink()
-
-# +
-key = "Makefile"
-
-path = pathlib.Path(upld.value[key]["path"])
-path
-# -
-
-up.value
-
-f = widgets.HBox(
-    [
-        DisplayObject.from_path("edit_grid.py"),
-        widgets.Textarea(placeholder="add caption"),
-        widgets.Button(
-            button_style="danger", icon="trash", layout=dict(BUTTON_MIN_SIZE)
-        ),
-    ]
-)
-widgets.VBox([f, f, f])
-
-sch["properties"]["files"]
-
-add_files(upld.value)
-
-# +
-
-f = open(pathlib.Path("sample.txt"), "wb")
-f. write(by)
-f. close()
-# -
-
-pathlib.Path("sample.txt").write_bytes(by)
-
-by = upld.value['README.txt']['content']
-
-di = upld.value['README.txt']['metadata']
-
-# +
-
-
-stringcase.camelcase('last_modified')
-
-# +
-
-    
-di['fdir'] = pathlib.Path('')
-FileMetadata(**di)
-# -
-
-
-
-# ?datetime.fromtimestamp
-
-def 
-
-# +
-
-        
-        
