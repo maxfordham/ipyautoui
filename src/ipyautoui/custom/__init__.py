2c66ce538543416d94d1694e03ad9020b616e672
jgunstone <gunstone.john@gmail.com>
2022-06-16 05:47:23 +0100

    black. removed ipyautoui.custom.grid

--- src/ipyautoui/custom/__init__.py
+++ src/ipyautoui/custom/__init__.py
@@ -1,13 +1,16 @@
 import sys
 import pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
 # ^ for dev only. TODO: comment out at build time
 
-from ipyautoui.custom.grid import Grid
 from ipyautoui.custom.filechooser import FileChooser
 from ipyautoui.custom.multiselect_search import MultiSelectSearch
 from ipyautoui.custom.iterable import Array, Dictionary
-#from ipyautoui.custom.modelrun import RunName
+
+# from ipyautoui.custom.modelrun import RunName
 from ipyautoui.custom.loadproject import LoadProject
 from ipyautoui.custom.save_button_bar import SaveButtonBar
-#from ipyautoui.custom.edit_menu import EditGrid  # TODO: make this work
+
+# from ipyautoui.custom.edit_menu import EditGrid  # TODO: make this work
+
