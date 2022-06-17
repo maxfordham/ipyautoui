d7a33e05684e7b4961bf5cd5ca4ac643a76db95a
jgunstone <gunstone.john@gmail.com>
2022-06-17 15:49:39 +0100

    updated logo

---
+++ docs/crop_logo.py
@@ -0,0 +1,28 @@
+"""simple script to process the logo.png image to remove all whitespace and 
+trim to minimum size.
+
+Returns:
+    logo.png: saves over logo.png file
+"""
+
+from PIL import Image
+
+
+def convertImage(img):
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+    img.putdata(newData)
+    return img
+
+
+img = Image.open("logo.png")
+img = convertImage(img)
+img.getbbox()
+im2 = img.crop(img.getbbox())
+im2.save("logo.png")
