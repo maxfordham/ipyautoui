9787977d1ad9315abbe9cb77f537b9715c630bd3
john <j.gunstone@maxfordham.com>
2022-06-22 17:41:00 +0100

    minor updates

--- src/ipyautoui/basemodel.py
+++ src/ipyautoui/basemodel.py
@@ -21,11 +21,9 @@ def file(self:Type[BaseModel], path: pathlib.Path, **json_kwargs):
     
 
 class BaseModel(BaseModel):
-    def file(self, path: pathlib.Path, **json_kwargs):
-        if 'indent' not in json_kwargs.keys():
-            json_kwargs.update({'indent':4})
-        path.write_text(self.json(**json_kwargs), encoding='utf-8')
         
     class Config:
         json_encoders = {pathlib.PurePosixPath:  str}
         arbitrary_types_allowed = True
+        
+setattr(BaseModel, 'file', file)
