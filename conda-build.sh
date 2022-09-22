096bcbb60d57b33970f56080d3178e8dbac8093e
jgunstone <gunstone.john@gmail.com>
2022-09-22 22:25:51 +0100

    up rev

--- conda-build.sh
+++ conda-build.sh
@@ -1,6 +1,6 @@
 #  note. commands below intended to be run line-by-line rather than as a script
 #        this is to be used when building locally.
 conda build conda.recipe
-cp /home/jovyan/miniconda3/envs/mf_base/conda-bld/linux-64/ipyautoui-0.2.4*.tar.bz2 /mnt/conda-bld/linux-64
-conda convert --platform all /mnt/conda-bld/linux-64/ipyautoui-0.2.4*.tar.bz2 --output-dir /mnt/conda-bld
+cp /home/jovyan/miniconda3/envs/mf_base/conda-bld/linux-64/ipyautoui-0.2.6*.tar.bz2 /mnt/conda-bld/linux-64
+conda convert --platform all /mnt/conda-bld/linux-64/ipyautoui-0.2.6*.tar.bz2 --output-dir /mnt/conda-bld
 conda index /mnt/conda-bld
