1c8ac64f862f7139a5fc640770dedda375af2d7e
jgunstone <gunstone.john@gmail.com>
2022-11-09 13:02:33 +0000

    🔧-added pytest config vars to `pyproject.toml`

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-import sys
-
-FDIR_ROOT = pathlib.Path(__file__).parents[1]
-FDIR_SRC = FDIR_ROOT / "src"
-sys.path.append(str(FDIR_SRC)) # append ipyautoui source
-# ^ for DEV only. comment out at build time.
