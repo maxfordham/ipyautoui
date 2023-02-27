674ec636ae64a05f7dd0e9d9b5e0ef44e04ad0cc
jgunstone <gunstone.john@gmail.com>
2023-02-27 13:24:43 +0000

    ✅ update tests

--- tests/test_autoipywidget.py
+++ tests/test_autoipywidget.py
@@ -13,7 +13,11 @@ import pytest
 from .constants import DIR_TESTS, DIR_FILETYPES
 
 from ipyautoui import AutoUi, AutoDisplay, AutoVjsf
-from ipyautoui.autoipywidget import AutoObject, demo_autoobject_form, AutoObjectShowRaw
+from ipyautoui.autoipywidget import (
+    AutoObject,
+    demo_autoobject_form,
+    AutoObjectFormLayout,
+)
 from ipyautoui.demo_schemas import RootEnum, RootArrayEnum
 from ipyautoui.test_schema import TestAutoLogicSimple
 import stringcase
@@ -162,17 +166,3 @@ class TestAutoObjectRowOrder:
         self.ui.order_can_hide_rows = False
         assert self.ui.order == self.ui.default_order
         assert len(self.ui.autowidget.children) == len(self.ui.default_order)
-
-
-class TestAutoObjectKwargs:
-
-
-    def test_show_raw(self):
-
-        ui_showraw = AutoObjectShowRaw(show_raw=True)
-        assert ui_showraw.bn_showraw.layout.display == "None"
-
-        # ui = AutoObject(TestAutoLogicSimple, show_raw=True)
-        # assert ui.bn_showraw.layout.display == "None"
-
-
