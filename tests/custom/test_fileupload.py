245ec5e89a0ff7f54ecd9f0a6af5755a08ec29fe
jgunstone <gunstone.john@gmail.com>
2023-03-09 15:04:41 +0000

    ✅ 🚑 hotfix test

--- tests/custom/test_fileupload.py
+++ tests/custom/test_fileupload.py
@@ -1,20 +1,10 @@
-from ipyautoui.custom.fileupload import File, FileUi, FileUploadToDir
+from ipyautoui.custom.fileupload import FilesUploadToDir
 import pathlib
 
-FDIR_TEST = pathlib.Path(__file__).parents[1] / 'testdata'
+FDIR_TEST = pathlib.Path(__file__).parents[1] / "testdata"
+
 
 class TestFileUi:
     def test_file_ui(self):
-        f = File(name="test_savebuttonbar.py")
-        fui = FileUi(f)
-        assert fui.show_caption == True
-        assert fui.caption.layout.display == None
-
-        fui.show_caption = False
-        assert fui.show_caption == False
-        assert fui.caption.layout.display == "None"
-
-        fui.show_caption = True
-        assert fui.caption.layout.display == ""
-
-# class TestFileUploadToDir:
+        f = FilesUploadToDir(["test_buttonbars.py"])
+        assert f.value == ["test_buttonbars.py"]
