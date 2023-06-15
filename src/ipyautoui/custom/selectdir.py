ed2c5acaf07190b167ba33c6677b53de2508c66b
jgunstone <gunstone.john@gmail.com>
2023-06-15 08:56:27 +0100

    no material change

--- src/ipyautoui/custom/selectdir.py
+++ src/ipyautoui/custom/selectdir.py
@@ -16,8 +16,8 @@
 # +
 # %run ../_dev_sys_path_append.py
 # %run __init__.py
-#
 # %load_ext lab_black
+
 from IPython.display import display
 from ipyautoui.basemodel import BaseModel
 from ipyautoui.basemodel import file
