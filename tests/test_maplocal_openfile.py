b4973589bda429c6378ff971b2c19854843ac962
Oliver Hensby <o.hensby@maxfordham.com>
2023-12-18 16:40:40 +0000

    ✨ Implement show_validation ✅ Comment out maplocal test as requires user specific config

--- tests/test_maplocal_openfile.py
+++ tests/test_maplocal_openfile.py
@@ -5,24 +5,25 @@ import typing as ty
 from maplocal import maplocal, openlocal, maplocal_openlocal_exists
 from maplocal.maplocal import MAPENV
 
-if maplocal_openlocal_exists():
-    class TestMAPENV:
-        def test_MAPENV(self):
-            assert MAPENV.MAPLOCAL_FROM == pathlib.PurePosixPath("/home")
-            assert MAPENV.MAPLOCAL_TO == pathlib.PureWindowsPath(
-                "//wsl.localhost/Ubuntu/home"
-            )
+# TODO: Fix test
+# if maplocal_openlocal_exists():
+#     class TestMAPENV:
+#         def test_MAPENV(self):
+#             assert MAPENV.MAPLOCAL_FROM == pathlib.PurePosixPath("/home")
+#             assert MAPENV.MAPLOCAL_TO == pathlib.PureWindowsPath(
+#                 "//wsl.localhost/Ubuntu/home"
+#             )
 
 
-    class TestEnv:
-        # @pytest.mark.usefixtures("mock_env_user")
-        def test_dev_env(self):
+#     class TestEnv:
+#         # @pytest.mark.usefixtures("mock_env_user")
+#         def test_dev_env(self):
 
-            assert isinstance(MAPENV.openpath, ty.Callable)
-            p = pathlib.Path(__file__).resolve()
-            assert maplocal(p) == pathlib.PureWindowsPath(
-                "//wsl.localhost/Ubuntu/home/jovyan/ipyautoui/tests/test_maplocal_openfile.py"
-            )
-            openlocal(p)
-else:
-    pass
+#             assert isinstance(MAPENV.openpath, ty.Callable)
+#             p = pathlib.Path(__file__).resolve()
+#             assert maplocal(p) == pathlib.PureWindowsPath(
+#                 "//wsl.localhost/Ubuntu/home/jovyan/ipyautoui/tests/test_maplocal_openfile.py"
+#             )
+#             openlocal(p)
+# else:
+#     pass
