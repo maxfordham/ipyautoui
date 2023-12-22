0186c114dd8175d6d62d721fc7ff6d739693fc95
Oliver Hensby <o.hensby@maxfordham.com>
2023-12-22 15:59:52 +0000

    🐛 Fix UrlImageLink ♻️ Refactor functionality
    🐛 Fix style classes
    
    🚑 Fix output voila issue for UrlImageLink

--- src/ipyautoui/custom/urlimagelink.py
+++ src/ipyautoui/custom/urlimagelink.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.0
+#       jupytext_version: 1.15.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -19,97 +19,85 @@
 
 from IPython.display import display, HTML, clear_output
 import ipywidgets as w
-import traitlets as t
+import traitlets as tr
 
 
+# +
 class UrlImageLink(w.HBox):
     """a simple widget (inherits an HBox) for creating a clickable
     url link from an image"""
 
-    url = t.Unicode()
-    path_image = t.Unicode()
-    width = t.Integer()
-    height = t.Integer()
-    tooltip = t.Unicode(default_value="")
-    font_size = t.Integer(allow_none=True)
-    description = t.Unicode(allow_none=True)
+    url = tr.Unicode()
+    path_image = tr.Unicode()
+    width = tr.Integer(default_value=200)
+    height = tr.Integer(default_value=200)
+    tooltip = tr.Unicode(default_value="")
+    font_size = tr.Integer(default_value=20)
+    description = tr.Unicode(allow_none=True)
+    whitespace = tr.Integer(default_value=20)
+
+    @tr.observe(
+        "url", "path_image", "width", "height", "tooltip", "font_size", "description"
+    )
+    def update_display(self, change):
+        if self.url == "":
+            self.layout.display = "none"
+        else:
+            self.layout.display = "flex"
+            self.out.layout.width = f"{self.width + self.font_size*len(self.description)*0.45}px"  # HOTFIX: ipywidget Output needs explicit width to work in voila. Remove once resolved
+            with self.out:
+                clear_output()
+                display(self.html)
 
     def __init__(
         self,
-        url,
-        path_image,
-        tooltip=None,
-        description=None,
-        font_size=None,
-        width=40,
-        height=40,
+        *args,
+        **kwargs,
     ):
-        super().__init__()
         self.out = w.Output()
-        # self._description = w.HTML(description)
+        super().__init__(*args, **kwargs)
         self.children = [self.out]
-        self.url = url
-        self.path_image = path_image
-        if tooltip is not None:
-            self.tooltip = tooltip
-        self.width = width
-        self.height = height
-        self.description = description
-        self.font_size = font_size
-        self._init_controls()
-        self.update("")
-
-    def _init_controls(self):
-        self.observe(
-            self.update,
-            names=[
-                "url",
-                "path_image",
-                "width",
-                "height",
-                "tooltip",
-                "font_size",
-                "description",
-            ],
-        )
 
     @property
     def html(self):
         self.html_image = f"""<img border="0" src="{self.path_image}"width={self.width} height={self.height}>"""
         if self.description is not None:
-            style = f"<style> p.ex1 {{font-size: {self.font_size}px;}}</style>"
             html = f"""<!DOCTYPE html>
                     <html>
-                        {style}
+                    <style>
+                      .aligned {{
+                      display: flex;
+                      align-items: center;
+                    }}
+                    .text {{
+                      font-size: {self.font_size}px;
+                      padding-left: {self.whitespace}px;
+                    }}
+                    </style>
+                    <body>
                         <a href="{self.url}" title="{self.tooltip}" target="_blank">
-                            <p class="ex1">
-                                {self.html_image}
-                                <b>{self.description}</b>
-                            </p>
+                            <div class="aligned">
+                                <div>
+                                    {self.html_image}
+                                </div>
+                                <span class=text>{self.description}</span>
+                            </div>
                         </a>
+                    </body>
                     </html>
                     """
         else:
             html = self.html_image
-
         return HTML(html)
-
-    def update(self, onchange):
-        if self.url == "":
-            self.layout.display = "None"
-        else:
-            self.layout.display = ""
-            with self.out:
-                clear_output()
-                display(self.html)
 
 
 if __name__ == "__main__":
     im = UrlImageLink(
-        "https://github.com/maxfordham/ipyautoui",
-        "https://github.com/maxfordham/ipyautoui/blob/main/docs/logo.png?raw=true",
+        url="https://github.com/maxfordham/ipyautoui",
+        path_image="https://github.com/maxfordham/ipyautoui/blob/main/docs/images/logo.png?raw=true",
+        description="<b>ipyautoui</b>",
+        width=150,
+        font_size=30,
+        whitespace=40,
     )
     display(im)
-# -
-
-
