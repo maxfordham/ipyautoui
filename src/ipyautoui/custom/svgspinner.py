15f2b4899f74b745a86514581d4cacfd37c75a76
jgunstone <j.gunstone@maxfordham.com>
2024-02-14 18:06:59 +0000

    add `SvgSpinner`. this is simpler than HaloNotebook as it effectively just displays a (moving) image.

---
+++ src/ipyautoui/custom/svgspinner.py
@@ -0,0 +1,67 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import ipywidgets as w
+import traitlets as tr
+from IPython.display import SVG, clear_output
+from ipyautoui._utils import show_hide_widget
+from ipyautoui.constants import PATH_SVG
+
+class SvgSpinner(w.VBox):
+    show = tr.Bool(default_value=True)
+    complete = tr.Bool(default_value=False)
+
+    @tr.observe("show")
+    def _show(self, change):
+        show_hide_widget(self, self.show)
+
+    @tr.observe("complete")
+    def _complete(self, change):
+        if self.complete:
+            self.display_done()
+        else:
+            self.display_spinner()
+
+    def __init__(self, path_svg=PATH_SVG):
+        super().__init__()
+        self.path_svg = path_svg
+        self.out = w.Output()
+        self.children = [self.out]
+        self.display_spinner()
+
+    def display_done(self):
+        with self.out:
+            clear_output()
+            display(w.HTML("✔"))
+
+    def display_spinner(self):
+        with self.out:
+            clear_output()
+            display(SVG(self.path_svg))
+
+if __name__ == "__main__":
+    spinner = SvgSpinner()
+    display(spinner)
+
+# %%
+if __name__ == "__main__":
+    spinner.complete = True
+
+# %%
+if __name__ == "__main__":
+    spinner.show = False
+
+# %%
