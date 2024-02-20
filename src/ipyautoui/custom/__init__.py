d381be6aa46712c4c0fdc655ae1d66d5d306976e
jgunstone <j.gunstone@maxfordham.com>
2024-02-20 18:15:32 +0000

    add lazy imports

--- src/ipyautoui/custom/__init__.py
+++ src/ipyautoui/custom/__init__.py
@@ -4,3 +4,6 @@ from ipyautoui.custom.iterable import AutoArray, Array, Dictionary
 from ipyautoui.custom.loadproject import LoadProject
 from ipyautoui.custom.buttonbars import SaveButtonBar
 from ipyautoui.custom.fileupload import FileUploadToDir, FilesUploadToDir
+from ipyautoui.custom.executetasks import ExecuteTasks, SelectAndExecute
+from ipyautoui.custom.svgspinner import SvgSpinner
+from ipyautoui.custom.filedownload import FileDownload, FilesDownload, SelectAndDownload
