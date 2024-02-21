a507981768fc2c37ab3b9931d75c65a0a532ea14
jgunstone <j.gunstone@maxfordham.com>
2024-02-21 08:37:02 +0000

    ✅update tests to ensure execution order is preserved

--- tests/custom/test_executetasks.py
+++ tests/custom/test_executetasks.py
@@ -4,13 +4,13 @@ from random import random
 from time import sleep
 import functools
 def task(s):
-    t = random() * 2
+    t =  s
     sleep(t)
     return f"sleep: {s}"
 
 def test_ExecuteTasks():
     END = 2
-    tasks = {f"task-{_}" : functools.partial(task, f"result-{_}") for _ in range(END)}
+    tasks = {f"task-{_}" : functools.partial(task, _) for _ in range(END)}
     ex = ExecuteTasks(tasks=tasks)
     ex.start()
-    assert ex.results == [f"sleep: result-{_}" for _ in range(END)]
+    assert ex.results == [f"sleep: {_}" for _ in range(END)]
